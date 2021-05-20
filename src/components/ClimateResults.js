236c2741459b0f34fdb104cca60e800b37213f9c
Austin Schrader <austin.jschrader@gmail.com>
2021-05-20 01:15:41 -0700

    Add material ui cards

--- src/components/ClimateResults.js
+++ src/components/ClimateResults.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Temp from './Temp';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const ClimateResults = (props) => {
-  return <div></div>;
+  return (
+    <div className='ClimateResults card'>
+      <Card>
+        <CardContent>
+          <div>{props.day}</div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default ClimateResults;
