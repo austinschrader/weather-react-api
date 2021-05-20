5e716a466b1e2b7061464233084aaef11117a4bb
Austin Schrader <austin.jschrader@gmail.com>
2021-05-20 00:46:37 -0700

    Add morning output field

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Temp.css';
 
 const Temp = (props) => {
-  return <div className='Temp'></div>;
+  return (
+    <div className='Temp'>
+      <p>
+        <span className='morn'>Morning: {props.mornTemp}&#176;</span>
+      </p>
+    </div>
+  );
 };
 
 export default Temp;
