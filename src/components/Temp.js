c0bbba2e080386abf41eb5388c0ad5666bf0091d
Austin Schrader <austin.jschrader@gmail.com>
2021-05-20 00:46:52 -0700

    Add noon and evening output

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -7,6 +7,13 @@ const Temp = (props) => {
       <p>
         <span className='morn'>Morning: {props.mornTemp}&#176;</span>
       </p>
+      <p>
+        <span className='day'>Noon: {props.dayTemp}&#176;</span>
+      </p>
+      <p>
+        <span className='night'>Evening: {props.nightTemp}&#176;</span>
+      </p>
+
     </div>
   );
 };
